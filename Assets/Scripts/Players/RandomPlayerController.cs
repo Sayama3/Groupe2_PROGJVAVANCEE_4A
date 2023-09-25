2349f628fe3ed2582ff1dd2d0917b1e7e3b85f5e
Eliot Froment <fromenteliot@gmail.com>
2023-09-25 12:00:42 +0200

    Changed RandomPlayerController.cs to work on fixed timestep

--- Assets/Scripts/Players/RandomPlayerController.cs
+++ Assets/Scripts/Players/RandomPlayerController.cs
@@ -2,30 +2,27 @@ using UnityEngine;
 
 public class RandomPlayerController : PlayerController
 {
-    private void Update()
+    private void FixedUpdate()
     {
-        if (GameManager.tick)
-        {
-            int randomAction = Random.Range(0, 5);
+        int randomAction = Random.Range(0, 5);
 
-            switch (randomAction)
-            {
-                case 0:
-                    PlaceBomb();
-                    break;
-                case 1:
-                    MoveLeft();
-                    break;
-                case 2:
-                    MoveRight();
-                    break;
-                case 3:
-                    MoveDown();
-                    break;
-                case 4:
-                    MoveUp();
-                    break;
-            }
+        switch (randomAction)
+        {
+            case 0:
+                PlaceBomb();
+                break;
+            case 1:
+                MoveLeft();
+                break;
+            case 2:
+                MoveRight();
+                break;
+            case 3:
+                MoveDown();
+                break;
+            case 4:
+                MoveUp();
+                break;
         }
     }
 }
