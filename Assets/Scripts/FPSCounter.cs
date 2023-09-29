4a40429d9ac8abbc2a53e39e743938d7c72f01ec
Eliot Froment <fromenteliot@gmail.com>
2023-09-29 17:23:30 +0200

    FPS counter

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,19 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour
+{
+    private TMP_Text text;
+    
+    void Start()
+    {
+        text = GetComponent<TMP_Text>();
+    }
+
+    private void Update()
+    {
+        float fps = 1.0f / Time.deltaTime;
+        text.text = fps.ToString("000") + " FPS";
+    }
+}
